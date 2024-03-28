c3ef7451fa5992f5f4b4d85ea1b99af76e7227d1
AyeshaSaddiqa20 <ayeshadar2016@gmail.com>
2024-03-28 09:10:41 +0100

    Applied Merge Variable refactoring in Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,11 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
         System.out.print("Enter student's name: ");
-        String studentName = inputScanner.nextLine();
-        System.out.print("Enter student's ID: ");
-        int studentIdentifier = inputScanner.nextInt();
+        String studentInfo = inputScanner.nextLine(); // Merged variable to store both name and ID
         inputScanner.close();
 
-        System.out.println("Student's name: " + studentName + ", Student's ID: " + studentIdentifier);
+        System.out.println("Student's name and ID: " + studentInfo);
     }
 }
