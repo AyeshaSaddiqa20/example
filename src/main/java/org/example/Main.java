f984691e93a5e9b57170d03510f294bfac424c92
AyeshaSaddiqa20 <ayeshadar2016@gmail.com>
2024-03-28 09:42:57 +0100

    variable type changed added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,16 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
         System.out.print("Enter student's name: ");
-        String studentFullName = inputScanner.nextLine();
-        String[] nameParts = studentFullName.split(" ");
-        String firstName = nameParts[0];
-        String lastName = nameParts.length > 1 ? nameParts[1] : "";
+        String studentName = inputScanner.nextLine();
         System.out.print("Enter student's ID: ");
-        int studentIdentifier = inputScanner.nextInt();
+        long studentIdentifier = inputScanner.nextLong(); // Changed variable type to long
         inputScanner.close();
 
-        System.out.println("Student's first name: " + firstName);
-        System.out.println("Student's last name: " + lastName);
-        System.out.println("Student's ID: " + studentIdentifier);
+        System.out.println("Student's name: " + studentName + ", Student's ID: " + studentIdentifier);
     }
 }
