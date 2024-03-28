bafcb74cc15f978c7b3de1dd940ac7e01f267bd3
AyeshaSaddiqa20 <ayeshadar2016@gmail.com>
2024-03-28 15:08:28 +0100

    Variable name changed

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -2,16 +2,16 @@ package org.example;
 // Extracted class Person from Student
 public class Person {
     private String first;
-    private String lastName;
+    private String last;
 
     // Extracted method to set person's name
     public void setName(String firstName, String lastName) {
         this.first = firstName;
-        this.lastName = lastName;
+        this.last = lastName;
     }
 
     // Extracted method to get person's name
     public String getName() {
-        return first + " " + lastName;
+        return first + " " + last;
     }
 }
