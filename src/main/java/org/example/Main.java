9566aa098232d2f5a6774ca9262fdad24b0e0d58
AyeshaSaddiqa20 <ayeshadar2016@gmail.com>
2024-04-18 10:27:55 +0200

    Last name changed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
         System.out.print("Enter student's name: ");
-        String studentName = inputScanner.nextLine();
+        String student_Name = inputScanner.nextLine();
         System.out.print("Enter student's ID: ");
         long studentIdentifier = inputScanner.nextLong(); // Changed variable type to long
         inputScanner.close();
 
-        System.out.println("Student's name: " + studentName + ", Student's ID: " + studentIdentifier);
+        System.out.println("Student's name: " + student_Name + ", Student's ID: " + studentIdentifier);
     }
 }
